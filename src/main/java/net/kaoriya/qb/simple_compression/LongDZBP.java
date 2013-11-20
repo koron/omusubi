535821623cd1a8d62ae05d054c23bcc49465c6ef
MURAOKA Taro <taro.muraoka@muraoka-design.com>
2013-11-20 14:16:16 +0900

    impl. delta zigzag encode and decode filters

--- src/main/java/net/kaoriya/qb/simple_compression/LongDZBP.java
+++ src/main/java/net/kaoriya/qb/simple_compression/LongDZBP.java
@@ -7,7 +7,59 @@ import java.nio.LongBuffer;
  */
 public class LongDZBP implements LongCompressor, LongDecompressor
 {
+    public static class DZEncodeFilter
+        extends DeltaZigzagEncoding.LongEncoder
+        implements LongFilter
+    {
+        private long savedContext = 0;
+
+        public long filterLong(long value) {
+            return encodeLong(value);
+        }
+
+        public void saveContext() {
+            this.savedContext = this.contextValue;
+        }
+
+        public void restoreContext() {
+            this.contextValue = this.savedContext;
+        }
+
+        public void resetContext() {
+            this.contextValue = 0;
+            this.savedContext = 0;
+        }
+    }
+
+    public static class DZDecodeFilter
+        extends DeltaZigzagEncoding.LongDecoder
+        implements LongFilter
+    {
+        private long savedContext = 0;
+
+        public long filterLong(long value) {
+            return decodeLong(value);
+        }
+
+        public void saveContext() {
+            this.savedContext = this.contextValue;
+        }
+
+        public void restoreContext() {
+            this.contextValue = this.savedContext;
+        }
+
+        public void resetContext() {
+            this.contextValue = 0;
+            this.savedContext = 0;
+        }
+    }
+
     private final LongBitPacking bitPack;
+
+    private final DZEncodeFilter encodeFilter = new DZEncodeFilter();
+
+    private final DZDecodeFilter decodeFilter = new DZDecodeFilter();
 
     public LongDZBP(LongBitPacking bitPack) {
         this.bitPack = bitPack;
@@ -23,9 +75,11 @@ public class LongDZBP implements LongCompressor, LongDecompressor
 
     public void compress(LongBuffer src, LongBuffer dst) {
         // TODO:
+        this.bitPack.compress(src, dst, this.encodeFilter);
     }
 
     public void decompress(LongBuffer src, LongBuffer dst) {
         // TODO:
+        this.bitPack.decompress(src, dst, this.decodeFilter);
     }
 }
--- src/test/java/net/kaoriya/qb/simple_compression/LongDZBPTest.java
+++ src/test/java/net/kaoriya/qb/simple_compression/LongDZBPTest.java
@@ -16,4 +16,36 @@ public class LongDZBPTest
         LongDZBP p2 = new LongDZBP(bitPack);
         assertEquals(bitPack, p2.getBitPacking());
     }
+
+    @Test
+    public void encodeFilter() {
+        LongDZBP.DZEncodeFilter ef = new LongDZBP.DZEncodeFilter();
+        assertEquals(2, ef.filterLong(1));
+        assertEquals(1, ef.getContextValue());
+        ef.saveContext();
+        assertEquals(18, ef.filterLong(10));
+        assertEquals(10, ef.getContextValue());
+
+        ef.restoreContext();
+        assertEquals(1, ef.getContextValue());
+
+        ef.resetContext();
+        assertEquals(0, ef.getContextValue());
+    }
+
+    @Test
+    public void decodeFilter() {
+        LongDZBP.DZDecodeFilter df = new LongDZBP.DZDecodeFilter();
+        assertEquals(1, df.filterLong(2));
+        assertEquals(1, df.getContextValue());
+        df.saveContext();
+        assertEquals(10, df.filterLong(18));
+        assertEquals(10, df.getContextValue());
+
+        df.restoreContext();
+        assertEquals(1, df.getContextValue());
+
+        df.resetContext();
+        assertEquals(0, df.getContextValue());
+    }
 }
