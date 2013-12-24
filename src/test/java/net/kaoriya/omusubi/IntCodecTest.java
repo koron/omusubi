69259df9d8546946ade2ba8426fd3bd6b5d70647
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-12-25 03:07:17 +0900

    add test classes for int variation

---
+++ src/test/java/net/kaoriya/omusubi/IntCodecTest.java
@@ -0,0 +1,60 @@
+package net.kaoriya.omusubi;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.nio.IntBuffer;
+
+public class IntCodecTest
+{
+    public static class ThroughCodec extends IntCodec {
+        public int compressedLen = 0;
+        public int decompressedLen = 0;
+
+        public void compress(IntBuffer src, IntOutputStream dst) {
+            int[] l = src.array();
+            dst.write(l);
+            this.compressedLen += l.length;
+        }
+
+        public void decompress(IntBuffer src, IntOutputStream dst) {
+            int[] l = new int[src.remaining()];
+            src.get(l);
+            dst.write(l);
+            this.decompressedLen += l.length;
+        }
+    }
+
+    @Test
+    public void compress() {
+        ThroughCodec c = new ThroughCodec();
+        byte[] b = c.compress(new int[] { 0, 1, 2, 3, 4, 5, 6, 7 });
+        assertArrayEquals(new byte[] {
+            0, 0, 0, 0,
+            0, 0, 0, 1,
+            0, 0, 0, 2,
+            0, 0, 0, 3,
+            0, 0, 0, 4,
+            0, 0, 0, 5,
+            0, 0, 0, 6,
+            0, 0, 0, 7,
+        }, b);
+        assertEquals(8, c.compressedLen);
+    }
+
+    @Test
+    public void decompress() {
+        ThroughCodec c = new ThroughCodec();
+        int[] l = c.decompress(new byte[] {
+            0, 0, 0, 0,
+            0, 0, 0, 1,
+            0, 0, 0, 2,
+            0, 0, 0, 3,
+            0, 0, 0, 4,
+            0, 0, 0, 5,
+            0, 0, 0, 6,
+            0, 0, 0, 7,
+        });
+        assertArrayEquals(new int[] { 0, 1, 2, 3, 4, 5, 6, 7 }, l);
+        assertEquals(8, c.decompressedLen);
+    }
+}
