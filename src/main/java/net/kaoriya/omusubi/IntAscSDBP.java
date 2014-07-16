3f5a4d94c5a0ae96ac8703a2ad6c0dc93b366b85
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-07-16 12:03:24 +0900

    union() trial version

--- src/main/java/net/kaoriya/omusubi/IntAscSDBP.java
+++ src/main/java/net/kaoriya/omusubi/IntAscSDBP.java
@@ -1,6 +1,9 @@
 package net.kaoriya.omusubi;
 
 import java.nio.IntBuffer;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.LinkedList;
 
 /**
  * Int Ascending Sorted Delta Bit Packing.
@@ -35,9 +38,72 @@ public class IntAscSDBP extends IntCodec
         return new IntDecompressStream(src, null, null);
     }
 
+    static class Reader {
+        IntInputStream stream;
+        Integer last = null;
+        Reader(IntInputStream stream) {
+            this.stream = stream;
+        }
+        Integer read() {
+            this.last = this.stream.read();
+            return this.last;
+        }
+        Integer last() {
+            return this.last;
+        }
+    }
+
+    static Reader newBytesDecompressReader(byte[] b) {
+        IntDecompressStream ds = new IntDecompressStream(
+                ByteBuffer.wrap(b).asIntBuffer(),
+                null,
+                new IntBitPacking());
+        Reader r = new Reader(ds);
+        r.read();
+        return r;
+    }
+
+    static Integer fetchMinimumInt(List<Reader> readers) {
+        Reader min = null;
+        for (Reader r : readers) {
+            Integer v = r.last();
+            if (v == null) {
+                continue;
+            }
+            if (min == null || v < min.last()) {
+                min = r;
+            }
+        }
+        if (min == null) {
+            return null;
+        }
+        // skip same or less values.
+        Integer minV = min.last();
+        for (Reader r : readers) {
+            Integer v = r.last();
+            while (v != null & v <= minV) {
+                v = r.read();
+            }
+        }
+        return minV;
+    }
+
     public static byte[] union(byte[] a, byte[] b, byte[] ...others) {
-        // TODO:
-        return null;
+        List<Reader> readers = new LinkedList<>();
+        readers.add(newBytesDecompressReader(a));
+        readers.add(newBytesDecompressReader(b));
+        for (byte[] c : others) {
+            readers.add(newBytesDecompressReader(c));
+        }
+        IntArrayOutputStream os = new IntArrayOutputStream();
+        while (true) {
+            Integer n = fetchMinimumInt(readers);
+            if (n == null) {
+                break;
+            }
+            os.write(n.intValue());
+        }
+        return toBytes(os.toIntArray());
     }
 
     public static byte[] intersect(byte[] a, byte[] b, byte[] ...others) {
